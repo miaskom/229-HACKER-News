68a3880ba453cdb47a9c2f36c69f89459b97ce87
M.Miasko <miaskom@gmail.com>
2020-06-30 10:33:18 +0200

    Initial Commit

---
+++ HACKER News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HACKER News
+//
+//  Created by Marcin Miasko on 30/06/2020.
+//  Copyright © 2020 Marcin Miasko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
